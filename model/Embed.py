1752cd59d0f5bbcd49908be14e4a93686f98f8e5
Nashwan Sabti <nash.sabti@gmail.com>
2024-05-07 17:09:31 -0400

    Add inverted embedding

---
+++ model/Embed.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class DataEmbedding_inverted(nn.Module):
+    def __init__(self, c_in, d_model, dropout=0.1):
+        super(DataEmbedding_inverted, self).__init__()
+        self.value_embedding = nn.Linear(c_in, d_model)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        x = self.value_embedding(x)
+        return self.dropout(x)
